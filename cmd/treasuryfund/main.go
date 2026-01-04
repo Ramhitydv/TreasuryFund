e7a4508162682448812ac7cc782842c807840f7a
Ramhit <118090677+Ramhitydv@users.noreply.github.com>
2026-01-04 19:56:44 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/treasuryfund/main.go
@@ -0,0 +1,23 @@
+// cmd/treasuryfund/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "treasuryfund/internal/treasuryfund"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := treasuryfund.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
